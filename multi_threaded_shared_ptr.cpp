7a1cd9c75c1d44b6e1765e1ac5d016f0d315b5e5
Ильин Максим <maxim.2638@gmail.com>
2023-09-04 18:14:49 +0300

    Добавьте файлы проекта.

---
+++ multi_threaded_shared_ptr.cpp
@@ -0,0 +1,12 @@
+﻿// multi_threaded_shared_ptr.cpp: определяет точку входа для приложения.
+//
+
+#include "multi_threaded_shared_ptr.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
